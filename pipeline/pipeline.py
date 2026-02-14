8501afd7780974f4628da48a6a359be204f883ee
Darlington Maposa <maposad04@gmail.com>
2026-02-14 11:25:14 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd    
+
+print("arguments", sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df["month"] = month
+print(df.head())
+
+df.to_parquet(f'output_month_{month}.parquet')
+
+print(f"Running pipeline for month {month}")
